b38308075a7f0f3828896f6c499abd775a4c8139
Diyorbek M <backenddevaloper@gmail.com>
2026-05-03 01:24:28 +0500

    fix: export PaymentStates from bot.states

--- bot/states/__init__.py
+++ bot/states/__init__.py
@@ -1,3 +1,3 @@
-from bot.states.states import RegisterStates, ProfileEditStates, OrderStates, ProductStates, MonitoringStates
+from bot.states.states import RegisterStates, ProfileEditStates, OrderStates, ProductStates, MonitoringStates, PaymentStates
 
-__all__ = ["RegisterStates", "OrderStates", "ProductStates", "MonitoringStates"]
+__all__ = ["RegisterStates", "OrderStates", "ProductStates", "MonitoringStates", "PaymentStates"]
